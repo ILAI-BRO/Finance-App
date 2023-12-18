b76e2aa9a3f802930328215c9405eda1f514cabb
Ilyas <ayderkhanov@yandex.ru>
2023-12-18 21:19:11 +0300

    I created project and added the first components

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from './components/pages/Main.jsx'
+
+function App() {
+	return (
+		<React.Fragment>
+			<Main></Main>
+		</React.Fragment>
+	)
+}
+
+export default App
---
+++ react-app/src/components/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Foot from '../views/global/Foot'
+import Head from '../views/global/Head'
+
+const Main = () => {
+	return (
+		<React.Fragment>
+			<Head></Head>
+			<Foot></Foot>
+		</React.Fragment>
+	)
+}
+
+export default Main
